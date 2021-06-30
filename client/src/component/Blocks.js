26d344523eaf8aac368933b3b17b454088a44644
LkingForW <45881727+LkingForW@users.noreply.github.com>
2021-06-30 15:52:26 -0400

    Update Blocks.js

--- client/src/component/Blocks.js
+++ client/src/component/Blocks.js
@@ -34,7 +34,7 @@ class Blocks extends Component {
 
               return (
                 <span key={key} onClick={this.fetchPaginatedBlocks(paginatedId)}>
-                  <Button size="small" variant="danger">
+                  <Button bsSize="small" bsStyle="danger">
                     {paginatedId}
                   </Button>{' '}
                 </span>
